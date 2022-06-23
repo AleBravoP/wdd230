5b8ee57fc566c1a4ce55b46164f8175badab071f
AleBravoP <bra21043@byui.edu>
2022-06-22 23:54:28 -0500

    Add Weather paige

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,3 @@
-//ADD the key and change units to imperial
 const apiURL = "//api.openweathermap.org/data/2.5/weather?id=5861900&appid=ec0583b433b1ae15d290f99d91af7edb&units=imperial"
 
 //Go fetch it and then wait for a response.
@@ -8,19 +7,12 @@ fetch(apiURL)
     //Once it comes back, display it to the console.
     console.log(weatherInfo);
     
-    document.getElementById('place').innerHTML=weatherInfo.name;
-    document.getElementById('currentTemp').innerHTML=weatherInfo.main.temp;
-    document.getElementById('windSpeed').innerHTML=weatherInfo.wind.speed;
+    document.getElementById('current-temp').innerHTML=weatherInfo.main.temp;
 
     const iconCode = weatherInfo.weather[0].icon;
-    console.log(iconCode);
-
     const icon_path = "//openweathermap.org/img/w/" + iconCode + ".png";
-    console.log(icon_path)
 
-    document.getElementById('weather_icon').src =icon_path;
+    document.getElementById('weather-icon').src =icon_path;
+    document.getElementById('weather-icon').alt = weatherInfo.weather[0].description;
 
  }); //end of "then" fat arrow function
-
-
-
