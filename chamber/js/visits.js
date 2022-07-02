66c64f1a7f1ddc2eba792e47ccc19b94e81b4e7e
AleBravoP <bra21043@byui.edu>
2022-07-02 14:03:22 -0500

    Add visit count

--- chamber/js/visits.js
+++ chamber/js/visits.js
@@ -1,102 +1,39 @@
-const previousDate = Number(localStorage.getItem("new-date"));
-console.log("previous date: " + previousDate + " milliseconds")
+let currentDate = Date.now();
+const visitCounts = document.querySelector(".visit-days");
 
-const prevDateDiv = (previousDate / 86400000);
-console.log("previous date divided: " + prevDateDiv + " days")
+const dayInMills = 24 * 60 * 60 * 1000;
 
-// Declare and instantiate a variable of type Date to hold the current date
-var date = Date.now();
-console.log("date: " + date + " milliseconds")
+const hours = (dateNum) => {
+    let date = new Date(dateNum);
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    let day = date.getDate();
+    return new Date(year, month, day);
+};
 
-const dateDivided = (date / 86400000);
-console.log("date divided: " + dateDivided + " days")
+if (!window.localStorage.getItem("previous-visit")) {
+    localStorage.setItem(
+      "previous-visit",
+      Number(hours(currentDate) - dayInMills)
+    );
+}
+if (!window.localStorage.getItem("last-visit")) {
+localStorage.setItem("last-visit", Number(hours(currentDate)));
+}
 
-const newDate = localStorage.setItem("new-date", date);
+const prevVisit = Number(window.localStorage.getItem("previous-visit"));
+const lastVisit = Number(window.localStorage.getItem("last-visit"));
 
-let datesDifference = (dateDivided - prevDateDiv);
-console.log("dates day difference: " + datesDifference + " days")
+let prevDay = new Date(prevVisit);
+let lastDay = new Date(lastVisit);
+let days = currentDate - lastDay;
 
-
-
-
-// let currentDate = Date.now();
-// const visitsDisplay = document.querySelector(".visit-days");
-
-// const dayMillis = 86400000;
-// console.log("oneDayInMills: " + dayMillis);
-
-// const zeroHours = (dateNum) => {
-//   let date = new Date(dateNum);
-//   let year = date.getFullYear();
-//   let month = date.getMonth();
-//   let day = date.getDate();
-//   return new Date(year, month, day);
-// };
-
-// console.log("zero hours:" + zeroHours(currentDate))
-// console.log("zero hrs minus dayMillis: " + zeroHours(currentDate) - dayMillis)
-
-
-
-
-// initialize display elements
-// const visitsDisplay = document.querySelector(".visit-days");
-
-// // get the stored value in localStorage
-// let numVisits = Number(localStorage.getItem("visits-ls"));
-
-// // determine if this is the first visit or display the number of visits.
-// if (numVisits !== 0) {
-// 	visitsDisplay.textContent = numVisits;
-// } else {
-// 	visitsDisplay.textContent = `This is your first visit!`;
-// }
-
-// // increment the number of visits.
-// numVisits++;
-// // store the new number of visits value
-// localStorage.setItem("visits-ls", numVisits);
-
-
-// store the new date
-// localStorage.setItem("new-date", date);
-
-// console.log(date - previousDate);
-
-// let dateDifference = date - previousDate;
-// console.log("Date Difference: " + dateDifference);
-
-// var dayMillis = 86400000;
-// var plusOneDayMillis = previousDate + dayMillis;
-// console.log("Today plus tomorrow: " + plusOneDayMillis)
-
-// if (previousDate !== 0) {
-
-//     // if (date >= plusOneDayMillis) {
-//     let daysSinceLastVisit = Math.floor(dateDifference / dayMillis);
-//     console.log("Days Since Last Visit:" + daysSinceLastVisit)
-//     visitsDisplay.textContent = daysSinceLastVisit;
-    // }
-
-    // if (dateDifference >= dayMillis) {
-    // let daysSinceLastVisit = Math.floor(dateDifference / dayMillis);
-    // console.log("Days Since Last Visit:" + daysSinceLastVisit)
-    // visitsDisplay.textContent = daysSinceLastVisit;
-    // }
-// } else {
-//     visitsDisplay.textContent = `0`;
-// };
-
-
-// let dateDifference = date - previousDate;
-// console.log("Date Difference: " + dateDifference);
-
-// var dayMillis = 86400000;
-
-// if (dateDifference >= dayMillis) {
-//     let daysSinceLastVisit = Math.floor(dateDifference / dayMillis);
-//     console.log(daysSinceLastVisit)
-//     visitsDisplay.textContent = daysSinceLastVisit;
-// } else {
-//     visitsDisplay.textContent = `0`;
-// };
+if (days > dayInMills) {
+    localStorage.setItem("previous-visit", Number(lastDay));
+    localStorage.setItem("last-visit", Number(hours(currentDate)));
+    let displayDays = Math.floor(days / dayInMills);
+    visitCounts.textContent = displayDays;
+} else {
+    let displayDays = Number((lastDay - prevDay) / dayInMills);
+    visitCounts.textContent = displayDays;
+}
