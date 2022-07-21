1e2611ebb2a18df24f72658fce84a5f5e3468e3d
AleBravoP <bra21043@byui.edu>
2022-07-20 23:50:46 -0500

    Services

--- temple_inn/js/main.js
+++ temple_inn/js/main.js
@@ -6,3 +6,14 @@ function toggleMenu() {
 
 const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
+
+// Declare and instantiate a variable of type Date to hold the current date
+var date = new Date();
+// Declare a variable to hold the current year then assign the year value to variable
+var year;
+year = date.getFullYear()
+document.getElementById("year").textContent = year;
+
+// Construct a variable to hold the last modification date and add it to the html doc
+const lastModif = new Intl.DateTimeFormat("en-US", { dateStyle: "full", timeStyle: "medium"}).format(date);
+document.querySelector("#modifDate").textContent = lastModif;
